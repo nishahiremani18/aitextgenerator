d467cc138661d392932c8ad7242067c08ae23c90
nisha hiremani <57809790+nishahiremani18@users.noreply.github.com>
2021-03-22 11:49:01 +0530

    Update textgen2.py

--- textgen2.py
+++ textgen2.py
@@ -8,17 +8,16 @@ st.title("AI content generator")
 ai = aitextgen()
 
 
-#prompt_text = "Python is awesome"
+
 prompt_text = st.text_input(label = "Enter your  text...",
-            value = "Computer is beautiful") #default value
+            value = "Computer is beautiful")
+prompt_number = st.number_input(label = "Enter a number",min_value=50, max_value=1000, value=100, step=50, format=None, key=prompt_text)
 
 with st.spinner("AI is at Work........"):
     # text generation
     gpt_text = ai.generate_one(prompt=prompt_text,
-            max_length = 150 )
+            max_length=prompt_number)
 st.success("AI Successfully generated the below text ")
 st.balloons()
 # print ai generated text
-print(gpt_text)
-
 st.text(gpt_text)
